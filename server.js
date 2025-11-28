4718a13b46345ab220f5bcd5ef735435ecf3e191
Navpreet Singh <navpreetg477@gmail.com>
2025-11-27 21:04:10 -0600

    adding server.js.

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = 3000;
+
+// scan again
+
+
+// trigger scan
+
+http.createServer((req, res) => {
+  const url = req.url;
+  //No   input validation on the prpvided url
+  fs.readFile("." + url, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200);
+    res.write(data);
+    return res.end();
+  });
+}).listen(port);
+
+console.log(`Server running at http://localhost:${port}`);
